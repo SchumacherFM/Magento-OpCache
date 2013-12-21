8542ab72423386d1846c35a986c837a3ac12fff2
Cyrill Schumacher <cyrill@schumacher.fm>
2013-12-21 20:12:28 +1100

    Initial Structure, setup ...

---
+++ src/app/code/community/SchumacherFM/OpCachePanel/Model/Types/Accelerator.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @category    SchumacherFM_OpCachePanel
+ * @package     Helper
+ * @author      Cyrill at Schumacher dot fm / @SchumacherFM
+ * @copyright   Copyright (c)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class SchumacherFM_OpCachePanel_Model_Types_Accelerator extends SchumacherFM_OpCachePanel_Model_Types_AbstractCache
+{
+
+}
---
+++ src/app/code/community/SchumacherFM/OpCachePanel/controllers/Adminhtml/CachePanelController.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @category    SchumacherFM_OpCachePanel
+ * @package     Controller
+ * @author      Cyrill at Schumacher dot fm / @SchumacherFM
+ * @copyright   Copyright (c)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class SchumacherFM_OpCachePanel_Adminhtml_CachePanelController extends Mage_Adminhtml_Controller_Action
+{
+    /**
+     * key list
+     *
+     * @return void
+     */
+    public function indexAction()
+    {
+        $this->_title($this->__('System'))->_title($this->__('OpCache Panel'));
+
+        $this->loadLayout();
+        $this->_setActiveMenu('system/cachepanel');
+        $this->renderLayout();
+    }
+
+    public function resetAction()
+    {
+
+    }
+
+    public function recheckAction()
+    {
+
+    }
+
+    /**
+     * @todo use http://snapsvg.io/ for chart generation
+     */
+    public function graphDataJsonAction(){
+
+    }
+
+    /**
+     * Check the permission to run it
+     *
+     * @return boolean
+     */
+    protected function _isAllowed()
+    {
+        return Mage::getSingleton('admin/session')->isAllowed('system/pgp');
+    }
+
+    /**
+     * Render grid
+     */
+    public function gridAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+}
