956c9a611ff7f2f791f0936db74f45fcb1a5d085
David Fuhr <davidfuhr@users.noreply.github.com>
2015-10-23 12:30:39 +0200

    Use magento default 404 page.

--- src/app/code/community/SchumacherFM/OpCachePanel/controllers/IndexController.php
+++ src/app/code/community/SchumacherFM/OpCachePanel/controllers/IndexController.php
@@ -9,17 +9,12 @@
  */
 class SchumacherFM_OpCachePanel_IndexController extends Mage_Core_Controller_Front_Action
 {
-
     public function indexAction()
     {
         $isKeyValid = Mage::helper('opcache')->isApiKeyValid($this->getRequest());
 
         if (!$this->getRequest()->isPost() || TRUE !== $isKeyValid) {
-            $this->getResponse()->setHeader('HTTP/1.1', '404 Not Found');
-            $this->getResponse()->setHeader('Status', '404 File not found');
-            $this->getResponse()->setBody('Status: 404 File not found');
-            $this->getResponse()->sendResponse();
-            exit;
+            return $this->_forward('defaultNoRoute');
         }
 
         Mage::getModel('opcache/cache')->reset();
