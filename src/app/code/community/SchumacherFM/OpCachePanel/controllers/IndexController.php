SchumacherFM/Magento-OpCache

commit 956c9a611ff7f2f791f0936db74f45fcb1a5d085

Use magento default 404 page.

src/app/code/community/SchumacherFM/OpCachePanel/controllers/IndexController.php
@@ -9,17 +9,12 @@
  */
 class SchumacherFM_OpCachePanel_IndexController extends Mage_Core_Controller_Front_Action
 {
-
     public function indexAction()
     {
         $isKeyValid = Mage::helper('opcache')->isApiKeyValid($this->getRequest());
 
         if (!$this->getRequest()->isPost() || TRUE !== $isKeyValid) {
-            $this->getResponse()->setHeader('HTTP/1.1', '404 Not Found');
-            $this->getResponse()->setHeader('Status', '404 File not found');
-            $this->getResponse()->setBody('Status: 404 File not found');
-            $this->getResponse()->sendResponse();
-            exit;
+            return $this->_forward('defaultNoRoute');
         }
 
         Mage::getModel('opcache/cache')->reset();

commit 99c49b0d8b7ac3235f7c5ab17763a0dbbd8c17b8

Fix for 'Front controller reached 100 router match iterations'
Fix for bug introduced in 956c9a611ff7f2f791f0936db74f45fcb1a5d085 on Magento 1.9.3.x.

src/app/code/community/SchumacherFM/OpCachePanel/controllers/IndexController.php
@@ -14,7 +14,7 @@ class SchumacherFM_OpCachePanel_IndexController extends Mage_Core_Controller_Fro
         $isKeyValid = Mage::helper('opcache')->isApiKeyValid($this->getRequest());
 
         if (!$this->getRequest()->isPost() || TRUE !== $isKeyValid) {
-            return $this->_forward('noRoute');
+            return $this->norouteAction();
         }
 
         Mage::getModel('opcache/cache')->reset();